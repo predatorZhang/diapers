d73d5f44729a8ca407b7f15859062f2840488bfd
Stevens Tian <yuant614@gmail.com>
2017-08-25 19:35:10 +0800

    v2.3
    implemented onekey share

--- app/src/main/java/com/worldlink/locker/activity/MainActivity.java
+++ app/src/main/java/com/worldlink/locker/activity/MainActivity.java
@@ -1,13 +1,19 @@
 package com.worldlink.locker.activity;
 
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -56,17 +62,25 @@ import org.androidannotations.annotations.ViewById;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.framework.utils.ShareSDKR;
 import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.wechat.friends.Wechat;
 
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
-
+    private static final String LOG_TAG = "MainActivity";
     //added by Stevens
 //    private SinkView sv_progress;
     private int[] bg_ids = {R.drawable.bg_hch0,
@@ -1105,7 +1119,12 @@ public class MainActivity extends BaseActivity {
         ib_share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showShare();
+                View mView = getWindow().getDecorView().getRootView();
+                mView.setDrawingCacheEnabled(true);
+                Bitmap bmp = Bitmap.createBitmap(mView.getDrawingCache());
+                mView.setDrawingCacheEnabled(false);
+                ScreenshotTask task = new ScreenshotTask();
+                task.execute(bmp);
             }
         });
 
@@ -1526,29 +1545,98 @@ public class MainActivity extends BaseActivity {
 
     }
 
-    private void showShare() {
+    private static final String SCREENSHOT_PATH = "/sdcard/Hainiu/";
+    private class ScreenshotTask extends AsyncTask<Bitmap, Void, String> {
+
+        @Override
+        protected String doInBackground(Bitmap... params) {
+
+            Bitmap bmp = params[0];
+            String root_path = SCREENSHOT_PATH;
+
+            File testPath = new File(root_path);
+            if(!testPath.exists()){
+                testPath.mkdir();
+            }
+            String filename = MainActivity.this.getString(R.string.share_file_name) + ".jpg";
+            File target = new File(SCREENSHOT_PATH + filename);
+            try {
+                FileOutputStream fos = new FileOutputStream(target);
+                bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.flush();
+                fos.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+
+            return filename;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s == null){
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.share_error_title);
+                builder.setMessage(R.string.share_error_fail_to_get_screenshot);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.create();
+                builder.show();
+
+            }else{
+                showShare("www.baidu.com", SCREENSHOT_PATH + s, getString(R.string.share_message));
+            }
+            super.onPostExecute(s);
+        }
+    }
+
+    private void showShare(String url, String image_path, String msg) {
         OnekeyShare oks = new OnekeyShare();
+        oks.setCallback(new PlatformActionListener() {
+            @Override
+            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+                Log.d(LOG_TAG, "onComplete");
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCancel(Platform platform, int i) {
+                Log.d(LOG_TAG, "onCancel");
+            }
+        });
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
-
         // 分享时Notification的图标和文字  2.5.9以后的版本不     调用此方法
         //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
         oks.setTitle(getString(R.string.share));
         // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl(url);
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText(getString(R.string.share_message));
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath("/sdcard/test.png");//确保SDcard下面存在此张图片
+        oks.setImagePath(image_path);//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
+        oks.setUrl(url);
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
+        oks.setComment(msg);
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
+        oks.setSiteUrl(url);
         // 启动分享GUI
         oks.show(this);
     }
